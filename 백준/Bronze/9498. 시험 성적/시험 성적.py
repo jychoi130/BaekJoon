3a58fd55e91e4d1f1133e6d63e24f47608b66561
jychoi130 <77255267+jychoi130@users.noreply.github.com>
2023-04-03 21:18:44 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,10 @@
+grade = int(input())
+if 90 <= grade <= 100:
+    print("A")
+elif 80 <= grade:
+    print("B")
+elif 70 <= grade:
+    print("C")
+elif 60<= grade:
+    print("D")
+else: print("F")
