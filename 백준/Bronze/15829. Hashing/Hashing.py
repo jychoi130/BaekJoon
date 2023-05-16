d8bfbb21e29445cde25ab6664ee78c3ced10816f
jychoi130 <77255267+jychoi130@users.noreply.github.com>
2023-05-16 21:13:46 +0900

    Update Hashing.py

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,13 +2,8 @@ L = int(input())
 s = input()
 
 ans = 0
-count = 0
 
-for i in s:
-    chk = 1
-    for j in range(count):
-        chk *= 31
-    ans += ((ord(i)-96)*chk)
-    count += 1
+for i in range(len(s)):
+    ans += ((ord(s[i])-96)*(31**i))
 
-print(ans)
+print(ans%1234567891)
